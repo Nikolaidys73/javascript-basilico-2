347571053d3101877acb68ac35f77e868319d143
Nikolaidys73 <112443645+Nikolaidys73@users.noreply.github.com>
2023-02-07 16:40:44 -0300

    Cambios correo

--- app.js
+++ app.js
@@ -258,24 +258,24 @@ function enviarCompra(e){
   //envio de correos ( respuesta)
   const btn = document.getElementById('button');
 
-  document.getElementById('form')
-  .addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    btn.value = 'Sending...';
-  
-    const serviceID = 'default_service';
-    const templateID = 'template_sizac6g';
-  
-    emailjs.sendForm(serviceID, templateID, this)
-      .then(() => {
-        btn.value = 'Send Email';
-        alert('Sent!');
-      }, (err) => {
-        btn.value = 'Send Email';
-        alert(JSON.stringify(err));
-      });
-  });
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_sizac6g';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Enviado!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
     
   const spinner = document.querySelector('#spinner')
   spinner.classList.add('d-flex')
